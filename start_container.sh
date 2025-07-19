a1a8b34c7073502b3ad5750e279874e8f963a17d
nidhi965 <90715820+nidhi965@users.noreply.github.com>
2025-07-19 13:09:09 +0000

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+r#!/bin/bash
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull nidhi965/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 nidhi965/simple-python-flask-app
